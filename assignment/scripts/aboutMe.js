174e81907df9a3c6926fc11926d3c59190793283
Munir Abshir <munirabshir@icloud.com>
2024-06-08 14:27:34 -0500

    switch

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -11,7 +11,7 @@ fullName = firstName + " " +lastName;
 console.log(fullName);
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
-let luckyNumber = 2;
+let luckyNumber = 7;
 console.log(luckyNumber)
 // 5 - Create a variable `introduction` and using the variables from above,
 //     give it the value of:
@@ -38,7 +38,7 @@ console.log(pets);
 
 // 9 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
 let friendsPets;
-friendsPets = 2;
+friendsPets = 3;
 
 // 10 - Add two pets to your `pets` variable
 pets = pets + 2
@@ -110,7 +110,17 @@ console.log(petStatus);
 
 let mostPets;
 
+if (pets > friendsPets) {
 
+  mostPets = pets;
+}
+
+if (friendsPets > pets) {
+
+  mostPets = friendsPets;
+}
+
+console.log(mostPets);
 
 
 // 16 - Make a variable called `luckyResult`
@@ -121,6 +131,23 @@ let mostPets;
 //      Otherwise, "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
 //      console.log `luckyResult` after the conditional has run.
+
+  
+
+let luckyResult;
+
+switch (luckyNumber){
+
+  case 1: luckyResult = 'First is the worst'
+  break;
+  case 2: luckyResult = 'Second is the best'
+  break;
+  case 3: luckyResult = 'Third is the one with the polka dot dress'
+  break;
+  default: luckyResult = 'Luck is what happens when preparation meets opportunity'
+}
+
+console.log(luckyResult);
 
 // 17 -- Rewrite question 12 with a `ternary` operator. You'll need to do research!
 
